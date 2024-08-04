4b60c38561f4da2401a989d96af49f240755e962
Jose Luis <jose.luis.sandoval@pittaj.com>
2024-08-04 07:43:05 -0600

    style: chage searchbox

--- packages/frontoffice/src/components/layout/SearchBox/SearchBox.tsx
+++ packages/frontoffice/src/components/layout/SearchBox/SearchBox.tsx
@@ -4,11 +4,10 @@ const SearchBox = () => {
 	return (
 		<section className="w-full bg-gray-200 py-8">
 			<div className="container mx-auto">
-				<h2 className="text-3xl font-bold text-center mb-4">Cuadro de Búsqueda</h2>
 				<div className="flex justify-center">
 					<input
 						type="text"
-						placeholder="Buscar..."
+						placeholder="Buscar el inmueble que necesitas"
 						className="border border-gray-300 rounded-lg p-2 w-full max-w-lg"
 					/>
 				</div>
